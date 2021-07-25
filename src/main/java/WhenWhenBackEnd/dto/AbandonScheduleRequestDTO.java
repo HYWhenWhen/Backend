a19245c60f329fe10be7b2feecc6e4882bf56438
Soonwoo Kwon <louis981220@gmail.com>
2021-07-25 18:07:55 +0900

    [feat] Repository 개발

---
+++ src/main/java/WhenWhenBackEnd/dto/AbandonScheduleRequestDTO.java
@@ -0,0 +1,13 @@
+package WhenWhenBackEnd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AbandonScheduleRequestDTO {
+    String idToken;
+    String scheduleKey;
+}
--- src/main/java/WhenWhenBackEnd/repository/MemberScheduleRepository.java
+++ src/main/java/WhenWhenBackEnd/repository/MemberScheduleRepository.java
@@ -1,13 +1,13 @@
 package WhenWhenBackEnd.repository;
 
-import WhenWhenBackEnd.domain.MemberSchedule;
-import WhenWhenBackEnd.domain.Schedule;
+import WhenWhenBackEnd.domain.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -24,6 +24,19 @@ public class MemberScheduleRepository {
 
     public void save(MemberSchedule memberSchedule) {
         em.persist(memberSchedule);
+    }
+
+    public List<Long> findByMemberIdAndScheduleID(String idToken, String scheduleKey) {
+        QMemberSchedule memberSchedule = QMemberSchedule.memberSchedule;
+
+        List<Long> findMemberSchedule = queryFactory
+                .select(memberSchedule.id)
+                .from(memberSchedule)
+                .where(memberSchedule.member.idToken.eq(idToken))
+                .where(memberSchedule.schedule.scheduleKey.eq(scheduleKey))
+                .fetch();
+
+        return findMemberSchedule;
     }
 
 }
