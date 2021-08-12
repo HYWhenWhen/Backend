7e2af89288bdeadb43d591eaf7d35dcb858be457
Soonwoo Kwon <louis981220@gmail.com>
2021-08-12 23:42:18 +0900

    develop 버전

--- src/main/java/WhenWhen/api/MemberApiController.java
+++ src/main/java/WhenWhen/api/MemberApiController.java
@@ -17,8 +17,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//@CrossOrigin(origins = "http://localhost:3000")
-@CrossOrigin(origins = "http://3.34.216.89/")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -58,7 +57,7 @@ public class MemberApiController {
     @PostMapping("/add-my-schedule")
     public AddMyScheduleResponseDTO addMySchedule(@RequestBody AddMyScheduleRequestDTO dto) {
         Member member = memberService.findOne(dto.getIdToken());
-        if (member == null)
+        if(member == null)
             return new AddMyScheduleResponseDTO(false);
 
         LocalDate localDate = dto.getLocalDate();
@@ -77,7 +76,7 @@ public class MemberApiController {
 
         PrivateDate privateDate = privateDateService.findOne(dto.getScheduleKey());
 
-        if (member == null || privateDate.getMember() != member) return new DeleteMyScheduleResponseDTO(false);
+        if(member == null || privateDate.getMember() != member)return new DeleteMyScheduleResponseDTO(false);
 
         privateDateService.delete(privateDate);
 
@@ -87,7 +86,7 @@ public class MemberApiController {
     @PostMapping("/get-my-schedule")
     public GetMyScheduleResponseDTO getMySchedule(@RequestBody GetMyScheduleRequestDTO dto) {
         Member member = memberService.findOne(dto.getIdToken());
-        if (member == null)
+        if(member == null)
             return new GetMyScheduleResponseDTO(null, false);
 
         List<PrivateDate> list = privateDateService.findByMember(member);
@@ -103,9 +102,8 @@ public class MemberApiController {
     @PostMapping("/get-my-page")
     public GetMyPageResponseDTO getMyPage(@RequestBody GetMyPageRequestDTO dto) {
         Member member = memberService.findOne(dto.getIdToken());
-        if (member == null)
-
-            return new GetMyPageResponseDTO(false, member.getIdToken(), member.getNickName(), null, null);
+        if(member == null)
+            return new GetMyPageResponseDTO(false, member.getIdToken(), member.getNickName(),null, null);
 
         List<PrivateDate> privateDateList = privateDateService.findByMember(member);
 
@@ -127,7 +125,7 @@ public class MemberApiController {
                 .map(schedule -> new SimpleScheduleDTO(schedule.getName(), schedule.getScheduleKey()))
                 .collect(Collectors.toList());
 
-        return new GetMyPageResponseDTO(true, member.getIdToken(), member.getNickName(), list, list3);
+        return new GetMyPageResponseDTO(true, member.getIdToken(), member.getNickName(),list, list3);
     }
 
     @PostMapping("/get-my-page-modal")
@@ -135,7 +133,7 @@ public class MemberApiController {
         Member member = memberService.findOne(dto.getIdToken());
         LocalDate localDate = dto.getLocalDate();
 
-        if (member == null)
+        if(member == null)
             return new GetMyPageModalResponseDTO(null, false);
 
         List<PrivateDate> privateDateList = privateDateService.findByMember(member);
@@ -156,7 +154,7 @@ public class MemberApiController {
         Member member = memberService.findOne(dto.getIdToken());
         Schedule schedule = scheduleService.findOne(dto.getScheduleKey());
 
-        if (member == null || schedule == null) return new AbandonResponseDTO(false);
+        if(member == null || schedule == null)return new AbandonResponseDTO(false);
 
         MemberSchedule memberSchedule = memberScheduleService.findByMemberAndSchedule(member, schedule);
 
--- src/main/java/WhenWhen/api/ScheduleApiController.java
+++ src/main/java/WhenWhen/api/ScheduleApiController.java
@@ -18,8 +18,7 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-//@CrossOrigin(origins = "http://localhost:3000")
-@CrossOrigin(origins = "http://3.34.216.89/")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
